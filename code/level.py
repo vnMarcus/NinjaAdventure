55b9663777b5c10b9b41a8b99a5b5ec3751aa94f
Vinhnd <92012621+vnMarcus@users.noreply.github.com>
2022-11-04 21:27:24 +0700

    create the player

--- code/level.py
+++ code/level.py
@@ -4,6 +4,7 @@ import pygame
 from settings  import  *
 from  tile import Tile
 from player import Player
+from debug import  debug
 class Level:
     def __init__(self):
         # get the display surface
@@ -21,9 +22,11 @@ class Level:
                 if col == 'x':
                     Tile(pos=(x, y),groups=[self.visible_sprites, self.obstacle_sprites])
                 if col == 'p':
-                    Player(pos=(x,y), groups=[self.visible_sprites])
+                    self.player = Player(pos=(x,y), groups=[self.visible_sprites], obstacle_sprites=self.obstacle_sprites)
 
     def run(self):
         #update and draw this game
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
 
