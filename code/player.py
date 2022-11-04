82de64965e9811828787e3b9a8733d771a16d114
Vinhnd <92012621+vnMarcus@users.noreply.github.com>
2022-11-04 17:03:52 +0700

    level setup

---
+++ code/player.py
@@ -0,0 +1,12 @@
+import pygame
+from settings import  *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        # Create an image of the block, and fill it with a color.
+        # This could also be an image loaded from the disk.
+        self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
+        # Fetch the rectangle object that has the dimensions of the image
+        # Update the position of this object by setting the values of rect.x and rect.y
+        self.rect = self.image.get_rect(topleft=pos)
